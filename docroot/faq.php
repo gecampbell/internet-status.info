728fdab51bcf4b9dd389d5fdeb782e53edb087e2
Glen Campbell <glen@broadpool.com>
2013-04-06 10:14:00 -0500

    added languages q

--- docroot/faq.php
+++ docroot/faq.php
@@ -16,36 +16,52 @@
         <div class="page-header">
         <h1>Frequently-Asked Questions</h1>
         </div>
+
     <h3>Why did you create this site?</h3>
     <p>So I'd know if the Internet was up.</p>
+
     <h3>How does it verify that the Internet is up?</h3>
     <p>By using HTTP over TCP/IP.</p>
+
     <h3>Can I donate to this project?</h3>
     <p>The thanks of a grateful world is all I need.</p>
+
     <h3>What do I do if the Internet is down?</h3>
     <p>Press the RESET button.</p>
+
     <h3>Does it really verify that the Internet is up?</h3>
     <p>Yes.</p>
+
     <h3>Really really?</h3>
     <p>Yes.</p>
+
     <h3>The whole thing?</h3>
     <p>Technically, it only verifies the portion of the Internet that
     is visible to you. But, really, that's the only part that counts,
     right?</p>
+
 	<h3>Do you ever add new features?</h3>
 	<p>Of course; what software developer doesn't allow feature creep?
 	You can check out the
 	<a href="http://x.internet-status.info">experimental
 	version of the site</a> for new features as they are added.</p>
+
 	<h3>Is this an open-source project?</h3>
 	<p>Yes. The source code is
 	<a href="https://github.com/gecampbell/internet-status.info">available
 	on Github</a>.</p>
+
 	<h3>Will you accept pull requests?</h3>
 	<p>Gladly. They may be subjected to a very high level of peer review
 	and analysis, however, given the strategic nature of the Internet.</p>
+
+	<h3>What languages is this available in?</h3>
+	<p>Currently: English, Esperanto, Spanish, Finnish, Norwegian, Danish,
+	German, French, Korean, and Chinese (Simplified).</p>
+
     <hr>
     <p><small><a href="/">Home</a></small></p>
+    <hr>
     </div>
     <div id="push"></div>
 </div>

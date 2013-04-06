5d073e4c123469ae370be8f6e1aa056b7a0a5ab0
Glen Campbell <glen@broadpool.com>
2013-04-06 09:59:45 -0500

    added FAQ

---
+++ docroot/faq.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Frequently-Asked Questions</title>
+  <meta charset="utf8">
+  <meta name="keywords" content="internet,status,uptime">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="http://fonts.googleapis.com/css?family=Special+Elite"
+        rel="stylesheet" type="text/css">
+  <link href="/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+  <link href="/css/site.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<div id="wrap">
+    <div class="container">
+        <div class="page-header">
+        <h1>Frequently-Asked Questions</h1>
+        </div>
+    <h3>Why did you create this site?</h3>
+    <p>So I'd know if the Internet was up.</p>
+    <h3>How does it verify that the Internet is up?</h3>
+    <p>By using HTTP over TCP/IP.</p>
+    <h3>Can I donate to this project?</h3>
+    <p>The thanks of a grateful world is all I need.</p>
+    <h3>What do I do if the Internet is down?</h3>
+    <p>Press the RESET button.</p>
+    <h3>Does it really verify that the Internet is up?</h3>
+    <p>Yes.</p>
+    <h3>Really really?</h3>
+    <p>Yes.</p>
+    <h3>The whole thing?</h3>
+    <p>Technically, it only verifies the portion of the Internet that
+    is visible to you. But, really, that's the only part that counts,
+    right?</p>
+    <hr>
+    <p><small><a href="/">Home</a></small></p>
+    </div>
+    <div id="push"></div>
+</div>
+<?php include 'footer.inc';?>
+</body>
+</html>
+<?php include 'ga.inc';?>

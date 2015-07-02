684a360eba21cf42035d27af6c565b03e5994d42
Glen Campbell <glen@broadpool.com>
2015-07-02 07:17:34 -0700

    fixed charset

--- push.sh
+++ push.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-aws s3 cp index.html s3://internet-status.info --cache-control 'no-cache, must-revalidate'
-aws s3 cp faq.html s3://internet-status.info --cache-control 'max-age=3600'
+aws s3 cp index.html s3://internet-status.info \
+	--cache-control 'no-cache, must-revalidate' \
+	--content-type 'text/html; charset=utf-8'
+aws s3 cp faq.html s3://internet-status.info \
+	--cache-control 'max-age=3600' \
+	--content-type 'text/html; charset=utf-8'
